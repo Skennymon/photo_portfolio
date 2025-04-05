7ba87773c294ee7372a56ef994d0ad3502c66126
Skennymon <77165403+Skennymon@users.noreply.github.com>
2025-04-05 00:13:39 -0700

    Finished responsive navbar

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,11 +1,21 @@
+import Image from "next/image"
+
 function Navbar() {
 
     return (
         <>
-            <nav className="flex border border-neutral-700 rounded-sm items-center justify-between p-4 font-main">
-                <h2 className="md:text-7xl sm:text-5xl text-3xl pl-2">Logo</h2>
-                <h2 className="md:text-7xl sm:text-5xl p-2">Kenny Photo Portfolio</h2>
-                <h2 className="md:text-7xl sm:text-5xl text-3xl pl-2">Hamburger Menu</h2>
+            <nav className="grid grid-cols-3 border border-neutral-700 rounded-sm p-4 font-main items-center">
+                
+                <div className="p-1 rounded-md relative w-10 h-10 lg:w-32 lg:h-32 md:w-20 md:h-20">
+                    <Image src="/camera.svg" alt="Camera" fill={true}/>
+                </div>
+                
+                <h1 className="md:text-7xl sm:text-5xl p-2 text-center">Kenny Photo Portfolio</h1>
+                
+                <div className="p-1 rounded-md relative w-10 h-10 lg:w-32 lg:h-32 md:w-16 md:h-16 justify-self-end">
+                    <Image src="/hamburger.svg" alt="Hamburger Menu" fill={true}/>
+                </div>
+                
             </nav>
         </>
     )
