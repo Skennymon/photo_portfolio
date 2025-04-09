ab2e2cde2d14a15446db1719f3ea247b2a8391f3
Skennymon <77165403+Skennymon@users.noreply.github.com>
2025-04-09 12:08:50 -0700

    Added placeholder stuff to add later

--- src/app/pages/about/page.js
+++ src/app/pages/about/page.js
@@ -1,6 +1,15 @@
 function About() {
     return (
-        <div>About Page</div>
+        <>
+            <div className="flex flex-col items-center justify-center mt-7">
+                
+                {/* add image of myself here */}
+                <section className="flex flex-col">
+                    <h1 className="font-main">About Me!</h1>
+                </section>
+            </div>
+        
+        </>
     )
 }
 
