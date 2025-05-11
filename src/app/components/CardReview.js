abdb8bc8003df3007355b1580ffd826496017240
Skennymon <77165403+Skennymon@users.noreply.github.com>
2025-05-11 16:17:37 -0700

    Added Marquee Slider

--- src/app/components/CardReview.js
+++ src/app/components/CardReview.js
@@ -1,13 +1,17 @@
 import Image from "next/image"
-function CardReview() {
+
+function CardReview({ msg, image }) {
+
+    console.log({image});
+
     return (
         <div className="flex items-center justify-between shadow md:gap-20 gap-2 h-36 md:h-[400px]">
             
             <div className="flex items-center justify-center w-[50%] relative h-full">
-                <Image src="/TroyMewing.jpg" alt="Brian" width={500} height={500}/>
+                <Image src={image} alt={msg} width={500} height={500}/>
             </div>
             <div className="flex flex-col gap-2 pr-7 md:items-left md:justify-center md:w-[50%]">
-                <p className="font-main text-2xl md:text-5xl lg:text-7xl">&quot;Bro made me look majestic&quot;</p>
+                <p className="font-main text-2xl md:text-5xl lg:text-7xl">&quot;{msg}&quot;</p>
                 <Image src="/yellow5star.png" width={250} height={250} alt="5 Stars"/>
             </div>
         </div>
--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,8 @@ import PhotoGallery from "./components/PhotoGallery";
 import CardReview from "./components/CardReview";
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
 import Autoplay from "embla-carousel-autoplay";
+import Link from 'next/link'
+import Marquee from "react-fast-marquee";
 
 export default function Home() {
   
@@ -31,7 +33,10 @@ export default function Home() {
     <>
       <div className="h-full w-full relative flex items-center justify-center">
         <Image src="/KennyAuraFarming.jpg" alt="Kenny Aura Farming" width={4907} height={4128}/>
-        <h1 className="absolute lg:text-7xl md:text-5xl text-2xl font-main">Welcome to my photography stuff</h1>
+        <div className="absolute flex flex-col items-center justify-center gap-7">
+          <h1 className="lg:text-7xl md:text-5xl text-2xl font-main">Welcome to my photography stuff</h1>
+          <Link className="w-[5rem] h-[3rem] md:w-[10rem] border rounded-2xl p-7 bg-emerald-300 font-main flex items-center justify-center" href="">Enter Portfolio</Link>
+        </div>
       </div>
 
       <section className="flex flex-col items-center justify-center mt-7 gap-2">
@@ -39,19 +44,32 @@ export default function Home() {
         
         <PhotoGallery photos={photos}/>
       </section>
+      
+      <section>
+        <Marquee>
+          <Image src="/SeabassTheThinker.jpg" alt="Seabass the Thinker" width={500} height={500}/>
+          <Image src="/Emily.jpg" alt="Emily" width={500} height={500}/>
+          <Image src="/GasStation.jpg" alt="Gas Station" width={500} height={500}/>
+          <Image src="/EpicCar.jpg" alt="Epic Car" width={500} height={500}/>
+          <Image src="/BlackCar2.jpg" alt="Epic Car" width={500} height={500}/>
+          <Image src="/RedCarEngine.jpg" alt="Epic Car" width={500} height={500}/>
+          
+        </Marquee>
+      </section>
 
       <section className="flex flex-col items-center justify-center mt-7 gap-2 p-7">
         <h2 className="text-2xl font-main md:text-5xl lg:text-7xl">Reviews</h2>
         <Carousel className="md:w-[90%] w-[75%]" plugins={[Autoplay({delay: 3000})]}>
           <CarouselContent className="-ml-4">
-            <CarouselItem className="pl-4"><CardReview/></CarouselItem>
-            <CarouselItem className="pl-4"><CardReview/></CarouselItem>
-            <CarouselItem className="pl-4"><CardReview/></CarouselItem>
+            <CarouselItem className="pl-4"><CardReview msg="Bro made me look majestic" image="/TroyMewing.jpg"/></CarouselItem>
+            <CarouselItem className="pl-4"><CardReview msg="Meh" image="/Emily.jpg"/></CarouselItem>
+            <CarouselItem className="pl-4"><CardReview msg="gg" image="/SeabassLayingDown.jpg"/></CarouselItem>
           </CarouselContent>
           <CarouselNext/>
           <CarouselPrevious/>
         </Carousel>
       </section>
+
 
     
     </>
