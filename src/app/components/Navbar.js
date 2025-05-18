468dcf00f5ac3b8f4a8325419ce10aa440cf977f
Skennymon <77165403+Skennymon@users.noreply.github.com>
2025-05-17 19:02:00 -0700

    Updated Navbar

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,6 +1,14 @@
+'use client'
 import Image from "next/image"
 import Link from "next/link"
+import { useState } from "react"
+import { useClickAway } from "@uidotdev/usehooks"
 function Navbar() {
+
+    const[dropDown, setDropdown] = useState(false);
+    const ref = useClickAway(() => {
+        setDropdown(false);
+    })
 
     return (
         <>
@@ -14,8 +22,15 @@ function Navbar() {
                 
                 <h1 className="md:text-7xl sm:text-5xl p-2 text-center">Kenny Photo Portfolio</h1>
                 
-                <div className="p-1 rounded-md relative w-10 h-10 lg:w-32 lg:hidden md:hidden md:h-16 justify-self-end">
-                    <Image src="/hamburger.svg" alt="Hamburger Menu" fill={true}/>
+                <div className="p-1 rounded-md relative w-10 h-10 lg:w-32 lg:hidden md:hidden md:h-16 justify-self-end" ref={ref}>
+                    <button className="cursor-pointer" onClick={() => setDropdown(!dropDown)}>
+                        <Image src="/hamburger.svg" alt="Hamburger Menu" fill={true}/>
+                    </button>
+                    <ul className={`flex flex-col border gap-4 w-56 absolute top-10 right-2 rounded-2xl items-center justify-center p-5 bg-gray-950 border-gray-600 shadow-gray-600 shadow transition-all duration-75 ease-in-out z-1 ${dropDown ? 'max-h-screen opacity-100' : 'max-h-0 opacity-0 pointer-events-none'}`}>
+                        <Link href="/pages/about" onClick={() => {setDropdown(false)}}>About me</Link>
+                        <Link href="/pages/about" onClick={() => setDropdown(false)}>Contact Me</Link>
+                        <Link href="/pages/about" onClick={() => setDropdown(false)}>Incquires</Link>
+                    </ul>
                 </div>
 
                 <div className="hidden md:flex md:items-center md:justify-end gap-7">
